363a0b43801125a64bd81017b1b735ef91c6197e
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-27 13:38:22 +0100

    DTO

---
+++ Sdt.Web.Api/Dto/AutorDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Sdt.Domain.Entities;
+
+namespace Sdt.Web.Api.Dto
+{
+    public class AutorDTO
+    {
+        public int AutorId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Beschreibung { get; set; }
+        public DateTime? Geburtsdatum { get; set; }
+
+        public virtual List<SpruchDTO> Sprueche { get; set; }
+
+        public byte[] Photo { get; set; }
+        public string PhotoMimeType { get; set; }
+    }
+}
---
+++ Sdt.Web.Api/Dto/SpruchDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Sdt.Domain.Entities;
+
+namespace Sdt.Web.Api.Dto
+{
+    public class SpruchDTO
+    {
+        public int SpruchId { get; set; }
+
+        [Required]
+        public string SpruchText { get; set; }
+        public string AutorName { get; set; }
+        public int AutorId { get; set; }
+    }
+}
