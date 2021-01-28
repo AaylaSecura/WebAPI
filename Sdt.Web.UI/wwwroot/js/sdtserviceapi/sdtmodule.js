a1a9707abb7ea35aa4d761d92f569e933f7f8964
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-28 15:33:02 +0100

    SdtModule

---
+++ Sdt.Web.UI/wwwroot/js/sdtserviceapi/sdtmodule.js
@@ -0,0 +1,12 @@
+﻿import { SpruchDesTages } from "./spruchdestagesmodule.js";
+
+const sdtUI = {
+    spruch: document.querySelector("#spruch"),
+    autorbild: document.querySelector("#bild"),
+    autorname: document.querySelector("#autor"),
+    autorbeschreibung: document.querySelector("#beschreibung"),
+    autorgeburtsdatum: document.querySelector("#geburtsdatum")
+};
+
+const apiUrl = "https://localhost:44319/api/sprueche/randomsdt";
+const spruchDesTages = new SpruchDesTages(sdtUI, apiUrl);
---
+++ Sdt.Web.UI/wwwroot/js/sdtserviceapi/spruchdestagesmodule.js
@@ -0,0 +1,56 @@
+﻿export class SpruchDesTages {
+    constructor(sdtUI, apiUrl) {
+        this.sdt = null;
+        this.sdtUI = sdtUI;
+        this.uri = apiUrl;
+
+        this.init();
+    }
+
+    init() {
+        this.getSdt().then(data => {
+            console.table(data);
+            this.sdt = data;
+            this.displayUI();
+        }).catch(this.handleErrors);
+    }
+
+    async getSdt() {
+        let response;
+
+        try {
+            response = await fetch(this.uri);
+
+            if (!response.ok) {
+                this.handleErrors(response.statusText);
+            }
+
+        } catch (e) {
+            this.handleErrors(e);
+        }
+
+        return await response.json();
+    }
+
+    displayUI() {
+        if (this.sdt) {
+            this.sdtUI.spruch.innerHTML = this.sdt.spruchText;
+            this.sdtUI.autorname.innerHTML = this.sdt.autorName;
+            this.sdtUI.autorbeschreibung.innerHTML = this.sdt.autorBeschreibung;
+
+            this.sdtUI.autorgeburtsdatum.innerHTML = this.sdt.autorGeburtsdatum
+                ? new Date(this.sdt.autorGeburtsdatum).toLocaleDateString()
+                : "k.A.";
+
+            this.sdtUI.autorbild.innerHTML = this.sdt.autorBildType
+                ? `<img src='data:${this.sdt.autorBildType};base64,${this.sdt.autorBild}' />`
+                : "<img src='https://via.placeholder.com/150' />";
+        } else {
+            this.handleErrors("Kein Spruch des Tages Objekt vorhanden");
+        }
+    }
+
+    handleErrors(errorMessage) {
+        console.error(errorMessage);
+    }
+}
