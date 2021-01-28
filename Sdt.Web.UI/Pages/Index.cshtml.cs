f765da540a26b1646c11df37cecec485180fd21d
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-28 13:27:39 +0100

    IOptions

--- Sdt.Web.UI/Pages/Index.cshtml.cs
+++ Sdt.Web.UI/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using Sdt.Web.UI.Model;
 
 namespace Sdt.Web.UI.Pages
@@ -13,15 +14,17 @@ namespace Sdt.Web.UI.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         public SpruchDesTagesViewModel SdtVm { get; set; }
+        public SdtAppSettings SdtAppSettings { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IOptions<SdtAppSettings> sdtAppSettings)
         {
             _logger = logger;
+            SdtAppSettings = sdtAppSettings.Value;
         }
 
         public void OnGet()
         {
-            //TODO: 
+            //TODO: Daten holen von webapi
         }
     }
 }
