8262fec19474515ed9a2a795a726c490868c7aee
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-28 12:16:25 +0100

    View SpruchDesTages

--- Sdt.Web.UI/Pages/Index.cshtml.cs
+++ Sdt.Web.UI/Pages/Index.cshtml.cs
@@ -5,12 +5,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Sdt.Web.UI.Model;
 
 namespace Sdt.Web.UI.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public SpruchDesTagesViewModel SdtVm { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
