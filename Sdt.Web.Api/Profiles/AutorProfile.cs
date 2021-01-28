372a87bf21f41241df0f9889b93445927525edff
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-28 09:47:50 +0100

    AutorProfile erweitert

--- Sdt.Web.Api/Profiles/AutorProfile.cs
+++ Sdt.Web.Api/Profiles/AutorProfile.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Sdt.Domain.Entities;
 using Sdt.Web.Api.Dto;
+using Sdt.Web.Common.Utilities;
 
 namespace Sdt.Web.Api.Profiles
 {
@@ -12,6 +13,24 @@ namespace Sdt.Web.Api.Profiles
     {
         public AutorProfile()
         {
+            CreateMap<AutorCreateDto, Autor>()
+                .ForMember(
+                    dest => dest.Photo,
+                    opt =>
+                    {
+                        opt.PreCondition(c => c.Photo != null);
+                        opt.MapFrom(src => Util.GetFile(src.Photo).Result.fileContent);
+                    })
+                .ForMember(
+                    dest => dest.PhotoMimeType,
+                    opt =>
+                    {
+                        opt.PreCondition(c => c.Photo != null);
+                        opt.MapFrom(src => src.Photo.ContentType);
+                    });
+
+
+
             CreateMap<Autor, AutorDTO>().ReverseMap();
         }
     }
