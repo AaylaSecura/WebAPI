66e4fd44772b885903f8d92fcc77cb27c424b8ba
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-28 10:16:06 +0100

    SpruchDesTagesDTO

---
+++ Sdt.Web.Api/Dto/SpruchDesTagesDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sdt.Web.Api.Dto
+{
+    public class SpruchDesTagesDTO
+    {
+        public int SpruchId { get; set; }
+
+        public string SpruchText { get; set; }
+
+        public int AutorId { get; set; }
+
+        public string AutorName { get; set; }
+
+        public string AutorBeschreibung { get; set; }
+
+        public DateTime? AutorGeburtsdatum { get; set; }
+
+        public byte[] AutorBild { get; set; }
+
+        public string AutorBildType { get; set; }
+    }
+}
